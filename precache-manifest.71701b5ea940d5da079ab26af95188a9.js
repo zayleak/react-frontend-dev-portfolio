16267e79d94bddcfdc052c71e5ece4bec0d54846
zayleak <victorghurst@gmail.com>
2023-12-06 19:16:45 -0500

    Updates

---
+++ precache-manifest.71701b5ea940d5da079ab26af95188a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46a16d70be0fcfcad8e756860ad2f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b20781fb01b761274d1c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cb7ad020780fb96ff28c",
+    "url": "/static/css/main.7083eeab.chunk.css"
+  },
+  {
+    "revision": "b20781fb01b761274d1c",
+    "url": "/static/js/2.41071e19.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.41071e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7ad020780fb96ff28c",
+    "url": "/static/js/main.72fb50b7.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
